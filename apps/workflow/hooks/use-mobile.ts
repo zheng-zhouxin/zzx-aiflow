308c1f5b414ab0c8e87c884cfcb921363d74fe7a
郑周鑫 <2539542593@qq.com>
2026-09-23 10:43:54 +0800

    fix: 🐛  修复提交问题

--- apps/workflow/hooks/use-mobile.ts
+++ apps/workflow/hooks/use-mobile.ts
@@ -2,20 +2,18 @@ import * as React from 'react'
 
 const MOBILE_BREAKPOINT = 768
 
-function subscribe(callback: () => void) {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    mql.addEventListener('change', callback)
-    return () => mql.removeEventListener('change', callbackd)
-}
-
-function getSnapshot() {
-    return window.innerWidth < MOBILE_BREAKPOINT
-}
-
-function getServerSnapshot() {
-    return false
-}
-
 export function useIsMobile() {
-    return React.useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot)
+    const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+
+    React.useEffect(() => {
+        const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
+        const onChange = () => {
+            setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+        }
+        mql.addEventListener('change', onChange)
+        setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+        return () => mql.removeEventListener('change', onChange)
+    }, [])
+
+    return !!isMobile
 }
